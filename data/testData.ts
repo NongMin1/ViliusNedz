204e21fae110f0de092fddfa758fb5480dee946e
Vilius Nedzveckas <viliusnedz@gmil.com>
2026-01-26 14:05:35 +0200

    Added all existing cattegories.

--- data/testData.ts
+++ data/testData.ts
@@ -2,4 +2,6 @@ export const SEARCH_CATEGORIES = {
   ALL: "All",
   WALLPAPERS: "Wallpapers",
   RINGTONES: "Ringtones",
+  NOTIFICATION_SOUNDS: "Notification Sounds",
+  ARTISTS: "Artists",
 } as const;
