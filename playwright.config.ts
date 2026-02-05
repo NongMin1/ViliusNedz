83ea475f651fb127c8e4dd5728822ae30e58839a
Vilius Nedzveckas <viliusnedz@gmil.com>
2026-02-05 13:35:03 +0200

    Removed duplicated code from config file.

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,19 +23,11 @@ export default defineConfig({
     {
       name: "ui",
       testDir: "./tests/ui",
-      use: {
-        baseURL: process.env.BASE_URL,
-        browserName: "chromium",
-        screenshot: "only-on-failure",
-        trace: "on-first-retry",
-        video: "retain-on-failure",
-      },
     },
     {
       name: "api",
       testDir: "./tests/api",
       use: {
-        baseURL: process.env.BASE_URL,
         extraHTTPHeaders: {
           "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
         },
